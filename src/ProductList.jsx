9e2e369a50cb02fefc27a4986c6f37799bcb72e4
heffs <stephenhefford@googlemail.com>
2025-02-15 14:34:06 +0000

    add functions for handling CartItem state

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -9,6 +9,8 @@ function ProductList() {
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
 
+    const dispatch = useDispatch();
+
     const handleAddToCart = (plant) => {
         dispatch(addItem(plant));
         const { name } = plant;
@@ -272,7 +274,6 @@ function ProductList() {
     };
 
     const handleContinueShopping = (e) => {
-        e.preventDefault();
         setShowCart(false);
     };
     return (
@@ -327,15 +328,15 @@ function ProductList() {
                                         height="156"
                                         fill="none"
                                     ></rect>
-                                    <circle cx="80" cy="216" r="12"></circle>
-                                    <circle cx="184" cy="216" r="12"></circle>
+                                    <circle cx="80" cy="216" r="16"></circle>
+                                    <circle cx="184" cy="216" r="16"></circle>
                                     <path
                                         d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8"
                                         fill="none"
                                         stroke="#faf9f9"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="6"
                                         id="mainIconPathAttribute"
                                     ></path>
                                 </svg>
